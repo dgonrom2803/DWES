4afd99f9cfec983a9d125b0ae28ef3fddd3859ab
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-09-20 14:52:49 +0200

    Segundo commit

---
+++ tema-01/ejemplos/02/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    /* 
+
+    Ejemplo 1 - PHP
+    Hola mundo
+
+    */
+    echo '<h1>';
+    echo '<hr>';
+    echo '<center>';
+    echo 'Muchas gracias afición';
+    echo '<BR>';
+    echo 'Soy Cristiano Ronaldo';
+    echo '</center>';
+    echo '<hr>';
+    echo '</h1>';
+    echo 'DWES - Curso 23/24';
+
+
+?>
