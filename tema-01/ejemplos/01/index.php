bdb88457d9e92625404dc8a966b9f95600d1f0e1
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-09-20 14:41:16 +0200

    Primer commit

---
+++ tema-01/ejemplos/01/index.php
@@ -0,0 +1,21 @@
+<?php
+
+    /* 
+
+    Ejemplo 1 - PHP
+    Hola mundo
+
+    */
+    echo '<h1>';
+    echo '<hr>';
+    echo '<center>';
+    echo 'Hola mundo PHP';
+    echo '<BR>';
+    echo 'Soy Diego González';
+    echo '</center>';
+    echo '<hr>';
+    echo '</h1>';
+    echo 'DWES - Curso 23/24';
+
+    
+?>
