cbb36151190af2217bc2881a96471c868e2622bf
dgonrom2803 <dgonrom2803@g.educaand.es>
2023-11-07 10:09:54 +0100

    Proyecto Calculadora. Finalizado

---
+++ tema-04/proyectos/01/calcular.php
@@ -0,0 +1,13 @@
+<?php
+
+#Clase calculadora
+include("class/class.calculadora.php");
+
+#Cargo el modelo
+include("models/model.calcular.php");
+
+#Cargo la vista
+include("views/view.resultado.php");
+
+
+?>
---
+++ tema-04/proyectos/01/index.php
@@ -0,0 +1,12 @@
+<?php
+
+#Clase calculadora
+include("class/class.calculadora.php");
+
+#Cargo el modelo
+
+#Cargo la vista
+include("views/view.index.php");
+
+
+?>
