1f356882424126a22e70e36f2b792de42ba0cfb8
dgonrom2803 <115530984+dgonrom2803@users.noreply.github.com>
2024-01-07 23:33:20 +0100

    Proyecto 6.1

---
+++ tema-06/proyectos/gesbank-mvc/config/config.php
@@ -0,0 +1,19 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+    // -> Tenemos en cuenta que debemos actualizar la URL que nos aparezca en nuestro localhost
+
+define('URL', 'http://localhost/dwes/DWES/tema-06/proyectos/gesbank-mvc/');
+
+# Constante de la Base de Datos
+    // -> Revisamos los datos de conexion a la BBDD
+    
+define('HOST', 'localhost');
+define('DB', 'gesbank');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
+
+
+?>
