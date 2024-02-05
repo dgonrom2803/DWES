09b3d0628fdb281adbb9c65f719a5267ffea060b
dgonrom2803 <dgonrom2803@g.educaand.es>
2024-02-05 14:56:38 +0100

    Proyecto Álbum

---
+++ tema-08/proyectos/01-album/config/config.php
@@ -0,0 +1,13 @@
+<?php
+# Configuración básica aplicación MVC
+
+# Ruta absoluta
+
+define('URL', 'http://localhost/dwes/tema-08/proyectos/01-album/');
+
+# Constante de la Base de Datos
+define('HOST', 'localhost');
+define('DB', 'album');
+define('USER', 'root');
+define('PASSWORD', '');
+define('CHARSET', 'utf8mb4');
